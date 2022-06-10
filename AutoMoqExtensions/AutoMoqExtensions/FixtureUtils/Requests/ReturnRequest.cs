f621b5722532930bd78b463d8bdbbee1b7c303fd
yoelhalb-hitech <yoelhalb@hitechmerchants.com>
2022-06-10 18:03:00 -0400

    WIP Adding generators

--- AutoMoqExtensions/AutoMoqExtensions/FixtureUtils/Requests/ReturnRequest.cs
+++ AutoMoqExtensions/AutoMoqExtensions/FixtureUtils/Requests/ReturnRequest.cs
@@ -3,25 +3,25 @@ using System.Collections.Generic;
 using System.Reflection;
 using System.Text;
 
-namespace AutoMoqExtensions.FixtureUtils.Requests
+namespace AutoMoqExtensions.FixtureUtils.Requests;
+
+internal class ReturnRequest : IEquatable<ReturnRequest>
 {
-    internal class ReturnRequest : IEquatable<ReturnRequest>
+    public ReturnRequest(Type declaringType, MethodInfo methodInfo)
     {
-        public ReturnRequest(Type declaringType, MethodInfo methodInfo)
-        {
-            DeclaringType = declaringType;
-            MethodInfo = methodInfo;
-        }
-
-        public Type DeclaringType { get; }
-        public MethodInfo MethodInfo { get; }
-
-        public override bool Equals(object obj) 
-            => obj is ReturnRequest other ? this.Equals(other) : base.Equals(obj);
-
-        public override int GetHashCode() => HashCode.Combine(DeclaringType, MethodInfo);
-
-        public virtual bool Equals(ReturnRequest other)
-            => other is not null && this.DeclaringType == other.DeclaringType && this.MethodInfo == other.MethodInfo;       
+        DeclaringType = declaringType;
+        MethodInfo = methodInfo;
     }
+
+    public Type DeclaringType { get; }
+    public MethodInfo MethodInfo { get; }
+
+    public override bool Equals(object obj) 
+        => obj is ReturnRequest other ? this.Equals(other) : base.Equals(obj);
+
+    public override int GetHashCode() => HashCode.Combine(DeclaringType, MethodInfo);
+
+    public virtual bool Equals(ReturnRequest other)
+        => other is not null && this.DeclaringType == other.DeclaringType && this.MethodInfo == other.MethodInfo;       
 }
+
