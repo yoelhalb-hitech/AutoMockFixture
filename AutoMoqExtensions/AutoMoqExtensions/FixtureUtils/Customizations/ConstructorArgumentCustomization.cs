708538c7d63b8927e4edf3170c758fdd7e047d0d
yoelhalb-hitech <yoelhalb@hitechmerchants.com>
2022-06-14 16:32:10 -0400

    Add constuctor argument option (some parts of it was already commited)

---
+++ AutoMoqExtensions/AutoMoqExtensions/FixtureUtils/Customizations/ConstructorArgumentCustomization.cs
@@ -0,0 +1,36 @@
+﻿using AutoFixture;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Text;
+
+namespace AutoMoqExtensions.FixtureUtils.Customizations
+{
+    public class ConstructorArgumentValue
+    {
+        public ConstructorArgumentValue(object? value, string? path = null)
+        {
+            Value = value;
+            Path = path;
+        }
+
+        public object? Value { get; }
+        public string? Path { get; }
+    }
+    public class ConstructorArgumentCustomization : ICustomization
+    {
+        public ConstructorArgumentValue ConstructorArgumentValue { get; }
+
+        public ConstructorArgumentCustomization(ConstructorArgumentValue constructorArgumentValue)
+        {
+            ConstructorArgumentValue = constructorArgumentValue;
+        }
+
+        public void Customize(IFixture fixture)
+        {
+            if (fixture is not AutoMockFixture mockFixture) throw new ArgumentException($"Expected {nameof(fixture)} to be `{nameof(AutoMockFixture)}`");
+
+            mockFixture.ConstructorArgumentValues.Add(ConstructorArgumentValue);
+        }
+    }
+}
--- AutoMoqExtensions/AutoMoqExtensions/FixtureUtils/Postprocessors/AutoMockConstructorArgumentPostprocessor.cs
+++ AutoMoqExtensions/AutoMoqExtensions/FixtureUtils/Postprocessors/AutoMockConstructorArgumentPostprocessor.cs
@@ -1,23 +1,43 @@
 ﻿using AutoFixture.Kernel;
 using AutoMoqExtensions.AutoMockUtils;
 using AutoMoqExtensions.AutoMockUtils.Specifications;
+using AutoMoqExtensions.Extensions;
+using AutoMoqExtensions.FixtureUtils.Customizations;
 using AutoMoqExtensions.FixtureUtils.Requests;
 using AutoMoqExtensions.FixtureUtils.Specifications;
 using System;
 using System.Collections.Generic;
+using System.Linq;
+using System.Reflection;
 using System.Text;
 
 namespace AutoMoqExtensions.FixtureUtils.Postprocessors
 {
     internal class AutoMockConstructorArgumentPostprocessor : ISpecimenBuilder
     {
+        public AutoMockConstructorArgumentPostprocessor(List<ConstructorArgumentValue> constructorArgumentValues)
+        {
+            ConstructorArgumentValues = constructorArgumentValues;
+        }
+
         private static readonly AutoMockableSpecification autoMockableSpecification = new();
+
+        public List<ConstructorArgumentValue> ConstructorArgumentValues { get; }
 
         public object? Create(object request, ISpecimenContext context)
         {
             if (request is not AutoMockConstructorArgumentRequest mockRequest) return new NoSpecimen();
 
             var type = mockRequest.ParameterInfo.ParameterType;
+
+            // Caution: Cannot just use FirstOrDefault and check for nullability as the custom value itself can be null
+            var hasCustomValue = ConstructorArgumentValues.Any(v => IsValidArgument(type, v, mockRequest.Path));
+            if(hasCustomValue)
+            {
+                var customValue = ConstructorArgumentValues.Any(v => IsValidArgument(type, v, mockRequest.Path));
+                mockRequest.SetResult(customValue);
+                return customValue;
+            }
 
             if (!autoMockableSpecification.IsSatisfiedBy(type))
             {
@@ -37,6 +57,14 @@ namespace AutoMoqExtensions.FixtureUtils.Postprocessors
             mockRequest.Completed();
             return specimen;
         }
+
+        private bool IsValidArgument(Type type, ConstructorArgumentValue argumentValue, string path)
+        {
+            if (argumentValue.Path is not null && argumentValue.Path != path) return false;
+
+            if (argumentValue.Value is not null) return type.IsInstanceOfType(argumentValue.Value);
+
+            return type.IsNullAllowed();
         }
     }
 }
