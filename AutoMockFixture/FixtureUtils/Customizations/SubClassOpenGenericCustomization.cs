b78333d576b9d2fa0e52ee667354ec94863ad9f7
yoelhalb-hitech <yoelhalb@hitechmerchants.com>
2023-08-15 19:17:13 -0400

    Bump versions

--- AutoMockFixture/FixtureUtils/Customizations/SubClassOpenGenericCustomization.cs
+++ AutoMockFixture/FixtureUtils/Customizations/SubClassOpenGenericCustomization.cs
@@ -8,11 +8,17 @@ namespace AutoMockFixture.FixtureUtils.Customizations;
 /// <summary>
 /// Customization for open generic types to replace with a derived type
 /// </summary>
-/// <remarks>Since the compiler won't allow an open generic type you should pass a closed type but the generic argumetns will be ignored</remarks>
+/// <remarks>NOTE: Since the compiler won't allow an open generic type you should pass a closed type but the generic arguments will be ignored</remarks>
 /// <typeparam name="TOriginal">Stub generic type for the original, the generic parameters will be ignored</typeparam>
 /// <typeparam name="TSubClass">Stub generic type for the subclass, the generic parameters will be ignored</typeparam>
 public class SubClassOpenGenericCustomization<TOriginal, TSubClass> : SubClassTransformCustomization
 {
+    /// <summary>
+    /// Customization for open generic types to replace with a derived type
+    /// </summary>
+    /// <remarks>NOTE: Since the compiler won't allow an open generic type you should pass a closed type but the generic arguments will be ignored</remarks>
+    /// <typeparam name="TOriginal">Stub generic type for the original, the generic parameters will be ignored</typeparam>
+    /// <typeparam name="TSubClass">Stub generic type for the subclass, the generic parameters will be ignored</typeparam>
     public SubClassOpenGenericCustomization() : base(typeof(TOriginal).GetGenericTypeDefinition(), typeof(TSubClass).GetGenericTypeDefinition())
     {
     }
--- CHANGELOG.md
+++ CHANGELOG.md
@@ -1,3 +1,16 @@
+## [1.1.5] - 2023-08-15
+
+### Added
+- Add support for transforming a base class to a dervied class via the `SubClassTransformCustomization`, `SubClassCustomization` and `SubClassOpenGenericCustomization` customizations
+- Add Changelog file
+
+### Changed
+- Change `FreezeCustomization` to public (this way it should be usable from the `UnitAutoDataAttribute` and `IntegrationAutoDataAttribute`, however currently it will require subclassing it to use it with the attributes)
+
+### Fixed
+- Fix regression with the `EnumerableBuilder` for a custom `IEnumerable<>` whcih is not abstract
+
+
 ## [1.1.4] - 2023-08-14
 
 ### Added
